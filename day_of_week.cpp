75bf900e587aff53d2d2fb55da7e4d542d8e833d
anupreeya24 <anupreeyawo@gmail.com>
2024-08-10 18:33:25 +0530

    Create day_of_week.cpp

---
+++ day_of_week.cpp
@@ -0,0 +1,43 @@
+//Anupreeya Bhattacharjee
+//23070123022
+#include<iostream>
+using namespace std;
+int main()
+    {
+int ch;
+cout<<"Enter your choice of week: "<<endl<<" 1.Monday 2.Tuesday 3.Wednesday 4.Thursday 5.Friday 6.Saturday 7.Sunday"<<endl;
+cin>>ch;
+switch(ch)
+{
+    case 1:
+    cout<<" Monday";
+    break;
+    case 2:
+    cout<<" Tuesday";
+    break;
+    case 3:
+    cout<<" Wednesday";
+    break;
+    case 4:
+    cout<<" Thursday";
+    break;
+    case 5:
+    cout<<" Friday";
+    break;
+    case 6:
+    cout<<" Saturday";
+    break;
+    case 7:
+    cout<<" Sunday";
+    break;
+
+}
+return 0;
+    }
+
+    /*Output 
+    Enter your choice of week: 
+ 1.Monday 2.Tuesday 3.Wednesday 4.Thursday 5.Friday 6.Saturday 7.Sunday
+ 2
+ Tuesday
+*/
